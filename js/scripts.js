461e8c76c48e4f8792df75d55d49786f1a7db625
kolmansoifer <kolmansoifer@gmail.com>
2020-08-13 09:54:40 -0700

    Change drinks project to voting project

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
 
-  if (age > 21) {
-    $('#drinks').show();
-  } else if (age === 21) {
-    alert("Now don't go crazy!");
-    $('#drinks').show();
+  if (age > 18) {
+    $('#votes').show();
+  } else if (age === 18) {
+    alert("Welcome to adulthood");
+    $('#votes').show();
   } else {
-    $('#under-21').show();
+    $('#under-18').show();
   }
 });
